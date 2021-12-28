1d3ec8a1edf37fd09d297e8973d1d2c31afae7f1
Steffen <kaemmi84@gmx.de>
2021-12-28 18:32:57 +0100

    init resource server

---
+++ relunchResource/src/main/kotlin/net/kaemmi/relunchResource/RelunchResourceApplication.kt
@@ -0,0 +1,11 @@
+package net.kaemmi.relunchResource
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RelunchResourceApplication
+
+fun main(args: Array<String>) {
+	runApplication<RelunchResourceApplication>(*args)
+}
---
+++ relunchResource/src/test/kotlin/net/kaemmi/relunchResource/RelunchResourceApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.kaemmi.relunchResource
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RelunchResourceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
